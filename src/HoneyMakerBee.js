9df1cea0aa0141c73c0462a5723dfaf1a8ad864f
matthewnguyen <itsmatthewnguyen@gmail.com>
2021-09-27 11:16:14 -0700

    Created makeHoney & giveHoney methods for HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -10,3 +10,11 @@ var HoneyMakerBee = function() {
 HoneyMakerBee.prototype = new Bee();
 
 HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
